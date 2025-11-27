e8c6be717c6425c555ccb86c55c2d7cc0c8a5bb8
Rahul Choudhary <2023aib1013@iitrpr.ac.in>
2025-11-27 18:55:26 +0530

    Prepare for Railway deployment

--- wsgi.py
+++ wsgi.py
@@ -3,10 +3,10 @@ print("WSGI starting...")
 from app import create_app
 
 print("Calling create_app()...")
+app = create_app()
+print("create_app() succeeded.")
 
-try:
-    app = create_app()
-    print("create_app() succeeded.")
-except Exception as e:
-    print("Error inside create_app():", e)
-    raise e
+if __name__ == "__main__":
+    # Force Flask dev server to run
+    app.run(host="127.0.0.1", port=5000, debug=True)
+
