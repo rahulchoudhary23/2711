8823dda6dd72d7211198c6b11d9a1a5b554cbf41
Rahul Choudhary <2023aib1013@iitrpr.ac.in>
2025-11-27 16:01:13 +0530

    Add debug logs to wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,16 @@
+print("WSGI starting...")
+
 from app import create_app
 
-app = create_app()
+print("Calling create_app()...")
 
+try:
+    app = create_app()
+    print("create_app() succeeded.")
+except Exception as e:
+    print("Error inside create_app():", e)
+    raise e
 
 if __name__ == "__main__":
     app.run(debug=True)
+
